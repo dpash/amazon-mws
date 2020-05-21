c67929105a01085909835ecdbed9683698d3a236
David Pashley <david@codingcraft.co.uk>
2020-05-21 13:24:07 +0200

    Handle error responses without throttle info headers
    It seems that error responses may not have throttle information in their headers
    This commit checks to make sure the headers exist before trying to use them. It
    also falls back to a fairly aggressive default.

--- src/Exceptions/QuotaExceededException.php
+++ src/Exceptions/QuotaExceededException.php
@@ -14,13 +14,17 @@ class QuotaExceededException extends MWSException
      */
     public function __construct(string $message, BadResponseException $exception) {
         parent::__construct($message, 0, $exception);
-        $this->quota_reset =  new \DateTime($exception->getResponse()->getHeader('x-mws-quota-resetsOn')[0]);
+        if ($exception->getResponse()->hasHeader('x-mws-quota-resetsOn')) {
+            $this->quota_reset = new \DateTime($exception->getResponse()->getHeader('x-mws-quota-resetsOn')[0]);
+        } else {
+            print_r($exception->getResponse()->getBody()->getContents());
+            $this->quota_reset = new \DateTime('now');
+        }
     }
 
     public function secondsToReset() : int {
         $now = new \DateTime('now');
         $interval = $this->quota_reset->diff($now);
-        print_r($interval);
         return $interval->s;
     }
 }
--- src/Exceptions/RequestThrottledException.php
+++ src/Exceptions/RequestThrottledException.php
@@ -12,6 +12,11 @@ class RequestThrottledException extends MWSException
     public function __construct(string $message, BadResponseException $exception) {
         parent::__construct($message, 0, $exception);
         print_r($exception->getResponse());
-        $this->quota_reset =  new \DateTime($exception->getResponse()->getHeader('x-mws-quota-resetsOn')[0]);
+        if ($exception->getResponse()->hasHeader('x-mws-quota-resetsOn')) {
+            $this->quota_reset = new \DateTime($exception->getResponse()->getHeader('x-mws-quota-resetsOn')[0]);
+        } else {
+            print_r($exception->getResponse()->getBody()->getContents());
+            $this->quota_reset = new \DateTime('now');
+        }
     }
 }
