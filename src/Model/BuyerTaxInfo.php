d236a5f686dadc7dd181652971db55f9c6651c6d
David Pashley <david@codingcraft.co.uk>
2020-06-04 10:23:52 +0200

    Parse GetOrder results into an object
    We were having issues with the response from Amazon being different based on
    whether it returned a single order or multiple orders.
    
    This handles this different transparently to the caller. Additionally, it
    converts the response into an object rather than an array.

---
+++ src/Model/BuyerTaxInfo.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Dpash\AmazonMWS\Model;
+
+
+class BuyerTaxInfo
+{
+
+    /**
+     * BuyerTaxInfo constructor.
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+    }
+}
---
+++ src/Model/PaymentMethodDetails.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Dpash\AmazonMWS\Model;
+
+
+class PaymentMethodDetails
+{
+    /**
+     * PaymentMethodDetail constructor.
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+    }
+}
